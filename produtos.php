3663c46dd87fb6103b9491ca72b660aaad7766dd
Luís Galete <luisgalete6264@gmail.com>
2020-10-25 15:27:27 -0300

    Add 'edit-product' option

--- produtos.php
+++ produtos.php
@@ -68,6 +68,27 @@ if (isset($_POST['submit-order'])) {
   mysqli_close($conn);
 }
 
+/* EDIT PRODUCT */
+if (isset($_POST['submit-edit-product'])) {
+  include 'config/connection.php';
+
+  $idEdit = $_POST['id-editar'];
+  $description = $_POST['descricao'];
+  $stock = $_POST['estoque'];
+  $value = $_POST['valor'];
+  $status = $_POST['status'];
+
+  if (mysqli_query($conn, "UPDATE produtos SET descricao = '$description', estoque = '$stock', valor = '$value', status = '$status' WHERE idproduto = '$idEdit'")) {
+    $_SESSION['finish-operation'] = ['type' => 'success', 'url' => 'produtos.php', 'text' => 'Produto editado com sucesso'];
+    header('location: templates/finish-operation.php');
+  } else {
+    $_SESSION['finish-operation'] = ['type' => 'error', 'url' => 'produtos.php', 'text' => 'Houve um problema ao editar o produto'];
+    header('location: templates/finish-operation.php');
+  }
+
+  mysqli_close($conn);
+}
+
 /* SEND PAGES */
 if (isset($_GET['cart'])) { /* Cart page */ ?>
   <?php
@@ -634,7 +655,62 @@ if (isset($_GET['cart'])) { /* Cart page */ ?>
     </body>
     </html>
   <?php } ?>
-<?php } else { /* Index page */ ?>
+<?php } else if (isset($_GET['edit-product'])) { /* Edit product page */ ?>
+  <?php
+  $idEdit = $_GET['id'];
+
+  include 'config/connection.php';
+
+  $res = mysqli_query($conn, "SELECT * FROM produtos WHERE idproduto = '$idEdit'");
+  $product = mysqli_fetch_assoc($res);
+  
+  mysqli_free_result($res);
+  mysqli_close($conn);
+  ?>
+  <!DOCTYPE html>
+  <html lang="pt-br">
+
+  <head>
+    <?php include 'templates/head.php'; ?>
+    <link rel="stylesheet" href="styles/pages/produtos.css">
+  </head>
+
+  <body id="edit-page">
+    <?php include 'templates/navbar.php'; ?>
+    <?php include 'templates/topbar.php'; ?>
+    <main>
+      <section class="product bg-light border rounded">
+        <svg viewBox="0 0 16 16" class="bi bi-pencil-fill" fill="#4B5C6B" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+        </svg>
+        <h2>Editar dados do produto</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+          <label for="descricao">Descrição</label>
+          <input type="text" name="descricao" id="descricao" class="form-control" placeholder="Descrição" value="<?php echo $product['descricao']; ?>" required>
+          <label for="estoque">Estoque</label>
+          <input type="number" name="estoque" id="estoque" class="form-control" placeholder="Estoque" value="<?php echo $product['estoque']; ?>" min="0" required>
+          <label for="valor">Valor</label>
+          <input type="number" name="valor" id="valor" class="form-control" placeholder="Valor" value="<?php echo $product['valor']; ?>" step="any" required>
+          <div class="radio-form">
+            <p>Status</p>
+            <div class="option">
+              <input type="radio" name="status" id="ativo" value="A" <?php if ($product['status'] == "A") { echo "checked"; } ?>>
+              <label for="ativo">Ativo</label>
+            </div>
+            <div class="option">
+              <input type="radio" name="status" id="inativo" value="I" <?php if ($product['status'] == "I") { echo "checked"; } ?>>
+              <label for="inativo">Inativo</label>
+            </div>
+          </div>
+          <input type="hidden" name="id-editar" value="<?php echo $product['idproduto']; ?>">
+          <button type="submit" name="submit-edit-product" class="btn btn-warning">Editar</button>
+        </form>
+      </section>
+    </main>
+  </body>
+
+  </html>
+<?php } else { /* Index page */ ?> 
   <?php
   /* ADD PRODUCTS TO CART */
   if (!isset($_GET['add-to-cart'])) {
@@ -732,6 +808,14 @@ if (isset($_GET['cart'])) { /* Cart page */ ?>
             <?php if ($gridCount < 4) { ?>
               <div class="product col-sm <?php if ($product['estoque'] > 0) { echo "border"; } ?> rounded">
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+                  <?php if ($_SESSION['priority'] >= 2) { ?>
+                  <a href="produtos.php?edit-product&id=<?php echo $product['idproduto']; ?>" class="edit-product">
+                    <svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="#72B7C1" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                    </svg>
+                  </a>
+                  <?php } ?>
                   <p class="h6"><?php echo $product['descricao']; ?></p>
                   <p>R$ <?php echo $product['valor']; ?></p>
                   <?php if ($product['estoque'] > 0) { ?>
@@ -784,6 +868,14 @@ if (isset($_GET['cart'])) { /* Cart page */ ?>
             <?php } else { ?>
               <div class="product col-sm <?php if ($product['estoque'] > 0) { echo "border"; } ?> rounded">
               <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+                <?php if ($_SESSION['priority'] >= 2) { ?>
+                  <a href="produtos.php?edit-product&id=<?php echo $product['idproduto']; ?>" class="edit-product">
+                    <svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="#72B7C1" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                    </svg>
+                  </a>
+                  <?php } ?>
                   <p class="h6"><?php echo $product['descricao']; ?></p>
                   <p>R$ <?php echo $product['valor']; ?></p>
                   <?php if ($product['estoque'] > 0) { ?>
@@ -841,5 +933,4 @@ if (isset($_GET['cart'])) { /* Cart page */ ?>
   </body>
 
   </html>
-
 <?php } ?>
