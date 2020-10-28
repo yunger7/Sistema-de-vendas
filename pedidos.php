5d12afbe0dc6c8bf5a010700d3b1de1178298815
Luís Galete <luisgalete6264@gmail.com>
2020-10-28 09:58:21 -0300

    Change edit-order style

--- pedidos.php
+++ pedidos.php
@@ -82,12 +82,12 @@ if (isset($_GET['view-order'])) { /* View order page */ ?>
     <?php include 'templates/head.php'; ?>
     <link rel="stylesheet" href="styles/pages/pedidos.css">
   </head>
-  <body id="view-order">
+  <body class="view-order">
     <?php include 'templates/navbar.php'; ?>
     <?php include 'templates/topbar.php'; ?>
     <main>
       <section class="order bg-light border rounded">
-        <svg width="1em" height="1em" viewBox="0 0 16 16" id="order-icon" class="bi bi-archive-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-archive-fill top-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
         </svg>
         <h2>Pedido nº <?php echo $order['idpedido']; ?></h2>
@@ -216,12 +216,12 @@ if (isset($_GET['view-order'])) { /* View order page */ ?>
     <?php include 'templates/head.php'; ?>
     <link rel="stylesheet" href="styles/pages/pedidos.css">
   </head>
-  <body id="view-order">
+  <body class="edit-order view-order">
     <?php include 'templates/navbar.php'; ?>
     <?php include 'templates/topbar.php'; ?>
     <main>
       <section class="order bg-light border rounded">
-        <svg viewBox="0 0 16 16" class="bi bi-pencil-fill" fill="#4B5C6B" xmlns="http://www.w3.org/2000/svg">
+        <svg viewBox="0 0 16 16" class="bi bi-pencil-fill top-icon" fill="#4B5C6B" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"></path>
         </svg>
         <h2>Pedido nº <?php echo $order['idpedido']; ?></h2>
@@ -269,9 +269,11 @@ if (isset($_GET['view-order'])) { /* View order page */ ?>
             <?php
             include 'config/connection.php';
 
-            $res = mysqli_query($conn, "SELECT pessoas.nome FROM pedidos JOIN clientes on pedidos.fk_idcliente = clientes.idcliente JOIN pessoas ON clientes.fk_idpessoa = pessoas.idpessoa WHERE pedidos.idpedido = '$id'");
+            $res = mysqli_query($conn, "SELECT idpessoa FROM pedidos JOIN clientes on pedidos.fk_idcliente = clientes.idcliente JOIN pessoas ON clientes.fk_idpessoa = pessoas.idpessoa WHERE pedidos.idpedido = '$id'");
+            $defaultClientId = mysqli_fetch_assoc($res);
 
-            $clientName = mysqli_fetch_assoc($res);
+            $res = mysqli_query($conn, "SELECT nome, idpessoa FROM clientes JOIN pessoas ON clientes.fk_idpessoa = pessoas.idpessoa");
+            $clients = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
             mysqli_free_result($res);
             mysqli_close($conn);
@@ -282,7 +284,15 @@ if (isset($_GET['view-order'])) { /* View order page */ ?>
               </svg>
               <p>Cliente</p>
             </div>
-            <span><?php echo $clientName['nome']; ?></span>
+            <select name="client" class="form-control">
+              <?php foreach($clients as $client) { ?>
+                <?php if ($client['idpessoa'] == $defaultClientId['idpessoa']) { ?>
+                  <option value="<?php echo $client['idpessoa'] ?>" selected><?php echo $client['nome']; ?></option>
+                <?php } else { ?>
+                <option value="<?php echo $client['idpessoa']; ?>"><?php echo $client['nome']; ?></option>
+                <?php } ?>
+              <?php } ?>
+            </select>
           </div>
           <div class="seller data">
             <?php
@@ -304,6 +314,7 @@ if (isset($_GET['view-order'])) { /* View order page */ ?>
             <span><?php echo $sellerName['nome']; ?></span>
           </div>
         </div>
+        <button type="submit" name="submit-edit-order" class="btn btn-warning">Editar</button>
       </section>
       <section class="products">
         <h2>Produtos</h2>
